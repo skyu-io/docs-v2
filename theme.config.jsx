d9ffb100d2ad5ae6990609a3b59507b2fabda4eb
nilesh93Insighture <140780155+nilesh93Insighture@users.noreply.github.com>
2024-08-27 12:52:11 +0530

    favicon

--- theme.config.jsx
+++ theme.config.jsx
@@ -17,6 +17,30 @@ export default {
 
         return (
             <>
+                <meta charset="UTF-8" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/favicon/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/favicon/site.webmanifest" />
+                <link
+                    rel="mask-icon"
+                    href="/favicon/safari-pinned-tab.svg"
+                    color="#5bbad5"
+                />
                 <meta property="og:url" content={url} />
                 <meta property="og:title" content={frontMatter.title || 'SkyU'} />
                 <meta
@@ -66,6 +90,9 @@ export default {
     sidebar: {
         defaultMenuCollapseLevel: 1,
         autoCollapse: false
+    },
+    nextThemes: {
+        defaultTheme: 'light'
     }
     // ... other theme options
 }
