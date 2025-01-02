7d6d220792340eb0a3c591099ee14f24d0680384
Hirusha Chamod <152847740+hirushaC@users.noreply.github.com>
2025-01-02 15:33:30 +0530

    fix site build issue

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,30 +1,34 @@
-// import CardContainer from "@/components/CardContainer";
-// import FAQSection from "@/components/FAQSection";
-// import FeatureArticles from "@/components/FeaturedArticles";
-// import Hero from "@/components/Hero";
-// import Wrapper from "@/components/Wrapper";
-// import { postQuery } from "@/lib/queries";
-// import client from "@/lib/sanityClient";
+import CardContainer from "@/components/CardContainer";
+import FAQSection from "@/components/FAQSection";
+import FeatureArticles from "@/components/FeaturedArticles";
+import Hero from "@/components/Hero";
+import Wrapper from "@/components/Wrapper";
+import { postQuery } from "@/lib/queries";
+import { client, isSanityConfigured } from "@/lib/sanityClient";
 
+export default function HomePage({articles}) {
+    return (
+      <Wrapper>
+        <Hero />
+        <CardContainer />
+        <FAQSection />
+        {isSanityConfigured && <FeatureArticles articles={articles} />}
+      </Wrapper>
+    );
+  }
+  
+export async function getStaticProps() {
 
-// export default function HomePage({articles}) {
-//     return (
-//       <Wrapper>
-//         <Hero />
-//         <CardContainer />
-//         <FAQSection />
-//         <FeatureArticles articles={articles} />
-//       </Wrapper>
-//     );
-//   }
-  
-//   export async function getStaticProps() {
-//     const articles = await client?.fetch(postQuery);
-  
-//     return {
-//       props: {
-//         articles,
-//       },
-//       revalidate: 60,
-//     };
-//   }
+  if (!isSanityConfigured) {
+    return { props: { data: null } };
+  }
+
+  const articles = await client?.fetch(postQuery);
+
+  return {
+    props: {
+      articles,
+    },
+    revalidate: 60,
+  };
+}
