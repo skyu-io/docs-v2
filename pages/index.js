4f59836079e98b177fa92a21f743ffed60170d62
Hirusha Chamod <152847740+hirushaC@users.noreply.github.com>
2025-04-16 11:31:38 +0530

    Add redirect from root to /docs page using Next.js router

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,10 @@
-import CardContainer from "@/components/CardContainer";
-import FAQSection from "@/components/FAQSection";
-import FeatureArticles from "@/components/FeaturedArticles";
-import Hero from "@/components/Hero";
-import Wrapper from "@/components/Wrapper";
-import { postQuery } from "@/lib/queries";
-import { client, isSanityConfigured } from "@/lib/sanityClient";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-export default function HomePage({ articles }) {
-
-  return (
-    <Wrapper>
-      <Hero />
-      <CardContainer />
-      <FAQSection />
-      {isSanityConfigured && <FeatureArticles articles={articles} />}
-    </Wrapper>
-  );
-}
-
-export async function getStaticProps() {
-  let articles = null;
-
-  if (isSanityConfigured && client) {
-    try {
-      articles = await client.fetch(postQuery);
-    } catch (error) {
-      console.error("Error fetching articles from Sanity:", error);
-      articles = null;
-    }
-  }
-
-  return {
-    props: {
-      articles,
-    },
-  };
+export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/docs");
+  }, [router]);
+  return null;
 }
